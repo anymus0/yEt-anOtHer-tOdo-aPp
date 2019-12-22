2197cd56bca5f0cc0719dbdee1894efac553f12a
Balazs Nyirfa <balazsnyirfa@gmail.com>
2019-12-22 13:39:49 +0100

    Output list of todo items and delete the items individually

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos 
+      v-bind:todos="todos"
+      v-on:del-todo="deleteTodo"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos,
+  },
+  //This is where data is stored
+  data() {
+    return {
+      //"todos" is an array of objects
+      todos: [
+        {id: 1, title: "Todo1", completed: false},
+        {id: 2, title: "Todo2", completed: false},
+        {id: 3, title: "Todo3", completed: false}
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, sans-serif;
+    line-height: 1.4;
+    background-color: #2c3e50;
+  }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- bind a class if the "completed" key's value is true -->
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+      {{todo.title}}
+    </p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .todo-item {
+    background: #27ae60;
+    padding: 10px;
+    border-bottom: 0.3rem #12181f solid;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: #851f1f;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 0%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
